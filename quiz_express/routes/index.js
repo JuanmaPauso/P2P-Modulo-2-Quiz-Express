e74f9d413300ce62e6fbbce58f1cb84d4447fa99
JuanmaPauso <juanmapauso@gmail.com>
2021-06-23 15:43:25 +0200

    Tarea 5

--- quiz_express/routes/index.js
+++ quiz_express/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+/* GET Credit page. */
+router.get("/credits", function(req, res, next) {
+    res.render("credits", { title: "Credits" });
 });
 
 module.exports = router;
