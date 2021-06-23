00a2ea74981ff4ba6a8b217db78620014f30ded0
JuanmaPauso <juanmapauso@gmail.com>
2021-06-23 15:58:38 +0200

    Tarea 6

--- quiz_express/routes/index.js
+++ quiz_express/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const quizControl = require("../controllers/quiz.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,8 @@ router.get('/', function(req, res, next) {
 router.get("/credits", function(req, res, next) {
     res.render("credits", { title: "Credits" });
 });
+
+/* GET Quizzes page. */
+router.get("/quizzes", quizControl.index);
 
 module.exports = router;
